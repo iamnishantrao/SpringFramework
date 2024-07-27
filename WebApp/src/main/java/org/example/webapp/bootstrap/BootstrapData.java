08117371c0595956249d6c1ad7eb388c76efade8
Nishant Yadav <nishantyadav025@gmail.com>
2024-07-27 13:20:04 -0700

    Add Publisher Entity and Repository

--- WebApp/src/main/java/org/example/webapp/bootstrap/BootstrapData.java
+++ WebApp/src/main/java/org/example/webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package org.example.webapp.bootstrap;
 
 import org.example.webapp.model.Author;
 import org.example.webapp.model.Book;
+import org.example.webapp.model.Publisher;
 import org.example.webapp.repositories.AuthorRepository;
 import org.example.webapp.repositories.BookRepository;
+import org.example.webapp.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,11 +15,13 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
     @Autowired
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -39,8 +43,23 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(dolores);
         bookRepository.save(westworld);
 
+        Publisher publisher = new Publisher("Spring Publishing", "3rd St", "San Jose", "CA", "95113");
+        publisherRepository.save(publisher);
+
+        springBoot.setPublisher(publisher);
+        publisher.getBooks().add(springBoot);
+
+        westworld.setPublisher(publisher);
+        publisher.getBooks().add(westworld);
+
+        bookRepository.save(springBoot);
+        bookRepository.save(westworld);
+        publisherRepository.save(publisher);
+
         System.out.println("Bootstrapping Test Data!");
         System.out.println("Number of books: " + bookRepository.count());
         System.out.println("Number of authors: " + authorRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Number of published books: " + publisher.getBooks().size());
     }
 }
