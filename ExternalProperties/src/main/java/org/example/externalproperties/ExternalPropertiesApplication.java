73388212d050227e9a72390685226342eb0243cb
Nishant Yadav <nishantyadav025@gmail.com>
2024-07-31 02:44:40 -0700

    Read External Properties from application.properties File

--- ExternalProperties/src/main/java/org/example/externalproperties/ExternalPropertiesApplication.java
+++ ExternalProperties/src/main/java/org/example/externalproperties/ExternalPropertiesApplication.java
@@ -1,5 +1,6 @@
 package org.example.externalproperties;
 
+import org.example.externalproperties.datasource.AppDataSource;
 import org.example.externalproperties.datasource.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +14,16 @@ public class ExternalPropertiesApplication {
         ApplicationContext ctx = SpringApplication.run(ExternalPropertiesApplication.class, args);
 
         DataSource dataSource = ctx.getBean(DataSource.class);
-        System.out.println("----------Data Source Details----------");
+        System.out.println("----------Data Source From datasource.properties----------");
         System.out.println(dataSource.getUsername());
         System.out.println(dataSource.getPassword());
         System.out.println(dataSource.getUrl());
+
+        AppDataSource appDataSource = ctx.getBean(AppDataSource.class);
+        System.out.println("\n----------Data Source From application.properties----------");
+        System.out.println(appDataSource.getUsername());
+        System.out.println(appDataSource.getPassword());
+        System.out.println(appDataSource.getUrl());
     }
 
 }
--- ExternalProperties/src/main/java/org/example/externalproperties/config/DataSourceConfiguration.java
+++ ExternalProperties/src/main/java/org/example/externalproperties/config/DataSourceConfiguration.java
@@ -1,5 +1,6 @@
 package org.example.externalproperties.config;
 
+import org.example.externalproperties.datasource.AppDataSource;
 import org.example.externalproperties.datasource.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,18 @@ public class DataSourceConfiguration {
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         dataSource.setUrl(url);
+
         return dataSource;
+    }
+
+    @Bean("appDataSource")
+    AppDataSource appDataSource(@Value("${app.username}") String username, @Value("${app.password}") String password,
+                                @Value("${app.url}") String url) {
+        AppDataSource appDataSource = new AppDataSource();
+        appDataSource.setUsername(username);
+        appDataSource.setPassword(password);
+        appDataSource.setUrl(url);
+
+        return appDataSource;
     }
 }
