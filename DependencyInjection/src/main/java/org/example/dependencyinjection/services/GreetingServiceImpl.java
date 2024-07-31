2aaa07164706e978fee981f11d019eb7bed4ff33
Nishant Yadav <nishantyadav025@gmail.com>
2024-07-30 20:46:24 -0700

    Added GreetingServiceImpl for JUnit Test Cases

---
+++ DependencyInjection/src/main/java/org/example/dependencyinjection/services/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package org.example.dependencyinjection.services;
+
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World!";
+    }
+}
