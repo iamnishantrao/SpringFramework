62c8c1aab57c971b9dee7fdfde58749acf117a91
Nishant Yadav <nishantyadav025@gmail.com>
2024-07-31 02:07:17 -0700

    Initialize External Properties Project

---
+++ ExternalProperties/src/main/java/org/example/externalproperties/ExternalPropertiesApplication.java
@@ -0,0 +1,13 @@
+package org.example.externalproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalPropertiesApplication.class, args);
+    }
+
+}
---
+++ ExternalProperties/src/test/java/org/example/externalproperties/ExternalPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.externalproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
